203e9e9fdf759736baf4a3bc902a18b2f5e0ecfd
Yanke Qiao <qiaoyanke@gmail.com>
2023-12-16 15:28:15 +0800

    only reply when mentioned in group

--- gemini_bot.py
+++ gemini_bot.py
@@ -7,7 +7,8 @@ from telebot.types import Message
 
 GOOGLE_GEMINI_KEY = environ.get("GOOGLE_GEMINI_KEY")
 genai.configure(api_key=GOOGLE_GEMINI_KEY)
-
+model = genai.GenerativeModel('gemini-pro')
+BOT_USERNAME = environ.get("BOT_USERNAME")
 botToken = environ.get("BOT_TOKEN")
 
 WHITELISTED_USERS = environ.get('WHITELISTED_USERS', '').split(',')
@@ -23,17 +24,37 @@ def is_user_allowed(message):
     str_chat_id = str(chat_id)
     return str_user_id in WHITELISTED_USERS or str_chat_id in WHITELISTED_GROUPS
 
+def is_bot_mentioned(message):
+    """检查消息中是否提及了机器人"""
+    return f"@{BOT_USERNAME}" in message.text if message.text else False
+
+def is_private_chat(message):
+    """检查是否为单聊"""
+    return message.chat.type == "private"
+
+def is_group_chat(message):
+    """检查是否为群聊"""
+    return message.chat.type in ["group", "supergroup"]
+
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "I'm Gemini, just ask me anything?")
+	bot.reply_to(message, "Hi, I'm Gemini, just ask me anything!")
 
-@bot.message_handler(func=lambda message: is_user_allowed(message))
+@bot.message_handler(func=lambda message: is_private_chat(message) and is_user_allowed(message))
 def echo_all(message):
-	# print(message.text)
-	model = genai.GenerativeModel('gemini-pro')
-	response = model.generate_content(message.text)
-	# print(response.text)
-	bot.reply_to(message, process_text_with_code(response.text), parse_mode="MarkdownV2")
+    print(message.text)
+    response = model.generate_content(message.text)
+    print(response.text)
+    bot.reply_to(message, process_text_with_code(response.text), parse_mode="MarkdownV2")
+
+@bot.message_handler(func=lambda message: is_group_chat(message) and is_user_allowed(message) and is_bot_mentioned(message))
+def echo_all_group(message):
+    print(message.text)
+    # model = genai.GenerativeModel('gemini-pro')
+    message_text = message.text.replace(f"@{BOT_USERNAME}", "").strip()
+    response = model.generate_content(message_text)
+    print(response.text)
+    bot.reply_to(message, process_text_with_code(response.text), parse_mode="MarkdownV2")
 	
 @bot.message_handler(func=lambda message: not is_user_allowed(message))
 def handle_unauthorized_messages(message):
